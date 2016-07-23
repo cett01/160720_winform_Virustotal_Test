fe5c50a49060ab839ada75f3e7dbf8a2df3d1f3f
cett01 <e.cihadcetin@gmail.com>
2016-07-23 14:10:27 +0300

    Virustotal Desktop Tool Simple
    This is a simple program for scanning files in the virustotal.com site.
    This program are build while using virustotal API

---
+++ Virustotal_Test/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Virustotal_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnGetReport = new System.Windows.Forms.Button();
+            this.lstViewScanResult = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(323, 48);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(94, 23);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "Choose File";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 18);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(58, 17);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "file path";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(475, 48);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(117, 23);
+            this.btnUpload.TabIndex = 2;
+            this.btnUpload.Text = "Upload / Scan";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnGetReport
+            // 
+            this.btnGetReport.Location = new System.Drawing.Point(403, 88);
+            this.btnGetReport.Name = "btnGetReport";
+            this.btnGetReport.Size = new System.Drawing.Size(91, 23);
+            this.btnGetReport.TabIndex = 3;
+            this.btnGetReport.Text = "Get Report";
+            this.btnGetReport.UseVisualStyleBackColor = true;
+            this.btnGetReport.Click += new System.EventHandler(this.btnGetReport_Click);
+            // 
+            // lstViewScanResult
+            // 
+            this.lstViewScanResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lstViewScanResult.FullRowSelect = true;
+            this.lstViewScanResult.GridLines = true;
+            this.lstViewScanResult.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstViewScanResult.Location = new System.Drawing.Point(15, 141);
+            this.lstViewScanResult.Name = "lstViewScanResult";
+            this.lstViewScanResult.Size = new System.Drawing.Size(645, 250);
+            this.lstViewScanResult.TabIndex = 5;
+            this.lstViewScanResult.UseCompatibleStateImageBehavior = false;
+            this.lstViewScanResult.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "State";
+            this.columnHeader2.Width = 50;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Report";
+            this.columnHeader3.Width = 400;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 435);
+            this.Controls.Add(this.lstViewScanResult);
+            this.Controls.Add(this.btnGetReport);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnChoose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Virus Total Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnGetReport;
+        private System.Windows.Forms.ListView lstViewScanResult;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Virustotal_Test/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirusTotalNET;
+using VirusTotalNET.Objects;
+
+namespace Virustotal_Test
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        VirusTotal virustotal = new VirusTotal("4f557e00e17d03c104aca2ab42245d2bfe9b488410289e6fdaf2c31143f83922");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+            }
+            lblPath.Text = path;
+
+
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+      
+                ScanResult myScanResult = virustotal.ScanFile(new System.IO.FileInfo(path));
+                MessageBox.Show(myScanResult.VerboseMsg);
+         
+            }
+            else
+            {
+                MessageBox.Show("You must select a file");
+            }
+            
+        }
+
+        private void btnGetReport_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                lstViewScanResult.Items.Clear();
+                FileReport fileReport = virustotal.GetFileReport(File.ReadAllBytes(path));
+                ReportResponseCode x= fileReport.ResponseCode;
+         
+                if ((long)x==-2)
+                {
+                    MessageBox.Show("Wait Please.  The requested item is still queued for analysis.");
+                }
+                  else if ((long)x==-1)
+                {
+                    MessageBox.Show("The item you searched for was not present in VirusTotal's dataset.");
+                }
+                     else if ((long)x==0)
+                {
+                    MessageBox.Show("The item you searched for was not present in VirusTotal's dataset.");
+                }
+                else if ((long)x ==1)
+                {
+                    foreach (var item in fileReport.Scans.Reverse())
+                    {
+                        ListViewItem temp = new ListViewItem();
+                        temp.Text = item.Key;
+                        temp.SubItems.Add(item.Value.Detected.ToString());
+                        temp.SubItems.Add(item.Value.Result);
+                        lstViewScanResult.Items.Add(temp);
+
+                    }
+                }
+                else
+                    MessageBox.Show("Again later.");
+            
+            }
+            else
+            {
+                MessageBox.Show("You must select a file");
+            }
+        }
+    }
+}
